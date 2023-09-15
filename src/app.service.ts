b3cfd4e04f76921357ef7e498cbc49a5c7454a81
jinhwan7 <rkflql851@gmail.com>
2023-09-15 23:25:07 +0900

    Prod 배포 테스트

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello hanghae ahahahahah';
+    return 'Hello hanghae plus this is production';
   }
 }
