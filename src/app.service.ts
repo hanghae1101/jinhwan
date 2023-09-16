a7ed241031b3065104658986fc8ef50589beafa1
jinhwan7 <rkflql851@gmail.com>
2023-09-16 17:00:02 +0900

    되나 확인

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello hanghae plus this is production';
+    return '이거 되는거 맞지?';
   }
 }
