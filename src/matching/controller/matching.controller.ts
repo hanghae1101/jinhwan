44e8dfc3894d0f903ffcfaa7578e0e85942ed7f6
Kong Jung Yong <menistream@gmail.com>
2023-10-21 00:03:44 +0900

    feat: matching (#53)

---
+++ src/matching/controller/matching.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post } from '@nestjs/common';
+import { MatchingService } from '../application/matching.service';
+
+@Controller('matching')
+export class MatchingController {
+	constructor(private readonly matchingService: MatchingService) {}
+
+	@Post()
+	matching(): Promise<any> {
+		return this.matchingService.matching();
+	}
+}
