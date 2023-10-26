8e4d053f797986f43bb48b30535983f801f26919
jinhwan7 <rkflql851@gmail.com>
2023-10-26 13:36:28 +0900

    test : bookmark 서비스 테스트코드 수정

--- src/find-way/application/bookmark.service.spec.ts
+++ src/find-way/application/bookmark.service.spec.ts
@@ -2,19 +2,22 @@ import { IBookmark } from '../domain/bookmark.interface';
 import { BookmarkService } from './bookmark.service';
 
 describe('', () => {
-  let service: BookmarkService;
+	let service: BookmarkService;
 
-  class mockBookmarkRepository implements IBookmark {
-    post(body: any): Promise<any> {
-      return Promise.resolve('success');
-    }
-    getAll(): Promise<any> {
-      throw new Error('Method not implemented.');
-    }
-  }
+	class mockBookmarkRepository implements IBookmark {
+		post(body: any): Promise<any> {
+			return Promise.resolve('success');
+		}
+		getAll(): Promise<any> {
+			throw new Error('Method not implemented.');
+		}
+		delete(id: number): Promise<any> {
+			return Promise.resolve('success');
+		}
+	}
 
-  it('should be created', () => {
-    service = new BookmarkService(new mockBookmarkRepository());
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		service = new BookmarkService(new mockBookmarkRepository());
+		expect(service).toBeTruthy();
+	});
 });
