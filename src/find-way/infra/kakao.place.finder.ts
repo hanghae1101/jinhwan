7e10d4bdd1d85d6fe5e3e06b61e546eda4d471ee
jinhwan7 <119090276+jinhwan7@users.noreply.github.com>
2023-10-26 13:39:23 +0900

    워크플로우 context 수정후 환경변수 파일 생성잘되나 확인 (#65)
    * refactor : tsconfig.json 수정, err객체에 타입 지정하라해서 "strict : false"로 변경,
    
    * refactor : prettier로 인한 코드 스타일변경
    
    * test : bookmark 서비스 테스트코드 수정
    
    * infra : ec2-deploy 워크플로우, 환경변수 context수정 env -> vars

--- src/find-way/infra/kakao.place.finder.ts
+++ src/find-way/infra/kakao.place.finder.ts
@@ -5,37 +5,37 @@ import { PlaceFinderOutputDto } from '../dtos/place.finder.output.dto';
 
 @Injectable()
 export class KakaoPlaceFinder implements PlaceFinder {
-  async findPlace(text: string): Promise<PlaceFinderOutputDto> {
-    async function KakaoLocalAPI(searchType: string, text: string) {
-      const url = `https://dapi.kakao.com/v2/local/search/${searchType}.json?analyze_type=similar&page=1&size=10&query=${text}`;
-      try {
-        const res = await axios
-          .get(url, {
-            headers: {
-              Authorization: `KakaoAK ${process.env.KAKAO_REST_API_KEY}`,
-            },
-          })
-          .then((res) =>
-            res.data.documents.map((obj) => {
-              return {
-                road_address: obj.road_address_name,
-                address: obj.address_name,
-                name: obj.place_name,
-                x: obj.x,
-                y: obj.y,
-              };
-            }),
-          );
+	async findPlace(text: string): Promise<PlaceFinderOutputDto> {
+		async function KakaoLocalAPI(searchType: string, text: string) {
+			const url = `https://dapi.kakao.com/v2/local/search/${searchType}.json?analyze_type=similar&page=1&size=10&query=${text}`;
+			try {
+				const res = await axios
+					.get(url, {
+						headers: {
+							Authorization: `KakaoAK ${process.env.KAKAO_REST_API_KEY}`,
+						},
+					})
+					.then((res) =>
+						res.data.documents.map((obj) => {
+							return {
+								road_address: obj.road_address_name,
+								address: obj.address_name,
+								name: obj.place_name,
+								x: obj.x,
+								y: obj.y,
+							};
+						})
+					);
 
-        return res;
-      } catch (err) {
-        throw new Error(err);
-      }
-    }
+				return res;
+			} catch (err) {
+				throw new Error(err);
+			}
+		}
 
-    const addressDocument = await KakaoLocalAPI('address', text);
-    const keywordDocument = await KakaoLocalAPI('keyword', text);
+		const addressDocument = await KakaoLocalAPI('address', text);
+		const keywordDocument = await KakaoLocalAPI('keyword', text);
 
-    return { addressDocument, keywordDocument };
-  }
+		return { addressDocument, keywordDocument };
+	}
 }
