1fc24ad2aaf78660920e14058707f87c2923efa0
gong-yeongbin <youngbin816@gmail.com>
2023-10-30 17:00:24 +0900

    feat(main): 유효성 검사 추가 (#109)
    class-validator class-transformer 설치

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT;
-  const app = await NestFactory.create(AppModule);
-  await app.listen(Number(port));
-  Logger.log(`port is ${port}`, 'application');
+	const port = process.env.PORT;
+	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+		})
+	);
+	await app.listen(Number(port));
+	Logger.log(`port is ${port}`, 'application');
 }
 bootstrap();
