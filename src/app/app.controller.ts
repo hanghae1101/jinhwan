544b2f160c34b592d1120c9356ff2ad5d28347f0
jinhwan7 <rkflql851@gmail.com>
2023-10-30 20:43:16 +0900

    infra : 배포테스트를 위한 app 컨트롤러 수정

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get()
-  getHello() {
-    return 'welcome!!, version : 0.0.0.1';
-  }
+	@Get()
+	getHello() {
+		return 'welcome!!, version : 0.0.0.2!!!';
+	}
 }
