e669d02d6a9d8745ea8ceb95eaac7047f0633d76
jinhwan7 <119090276+jinhwan7@users.noreply.github.com>
2023-10-30 20:34:00 +0900

    infra : 코드디플로이 관련 수정 (#111)
    docker-compose파일 경로 수정
    main.ts listen에 host 수정
    typeorm모듈 디비연결 host 수정

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,17 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
 	const port = process.env.PORT;
-	const app = await NestFactory.create(AppModule);
+	const host = process.env.HOST;
+  const app = await NestFactory.create(AppModule);
+	await app.listen(Number(port), host);
+
 	app.useGlobalPipes(
 		new ValidationPipe({
 			whitelist: true,
 			transform: true,
 		})
 	);
-	await app.listen(Number(port));
+  
 	Logger.log(`port is ${port}`, 'application');
 }
 bootstrap();
