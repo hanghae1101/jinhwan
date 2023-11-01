bec4acab7a9c7654669b3dd350cd03196d400282
gong-yeongbin <youngbin816@gmail.com>
2023-11-01 17:10:02 +0900

    feat(payment): payment controller 추가 (#119)
    payment controller 추가

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Post, Put, Query } from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { CreatePaymentDto } from './dtos/create.payment.dto';
+import { UpdatePaymentDto } from './dtos/update.payment.dto';
+import { DeletePaymentDto } from './dtos/delete.payment.dto';
 
 @Controller('payment')
-export class PaymentController {}
+export class PaymentController {
+	constructor(private readonly paymentService: PaymentService) {}
+
+	@Post()
+	create(@Body() createPaymentDto: CreatePaymentDto) {
+		this.paymentService.create(createPaymentDto);
+	}
+
+	@Put()
+	update(@Body() updatePaymentDto: UpdatePaymentDto) {
+		this.paymentService.update(updatePaymentDto);
+	}
+
+	@Delete()
+	delete(@Query() deletePaymentDto: DeletePaymentDto) {
+		this.paymentService.delete(deletePaymentDto);
+	}
+}
