bfe2cdbf4eda7f250a0ae0dbbbdeec3c01ca916c
xpecter <menistream@gmail.com>
2023-11-01 20:53:36 +0900

    feat: test logger

--- src/matching/application/matching.service.ts
+++ src/matching/application/matching.service.ts
@@ -1,19 +1,18 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { IMatching } from '../domain/matching.interface';
-import { WinstonLoggerService } from 'src/util/logger.service';
+import { CustomPinoLogger } from 'src/logger/application/logger.service';
 
 @Injectable()
 export class MatchingService {
 	constructor(
 		@Inject('MatchingRepository')
 		private readonly matchingRepository: IMatching,
-		private readonly logger: WinstonLoggerService
+		private readonly logger: CustomPinoLogger
 	) {}
 
 	async matching(): Promise<any> {
-		this.logger.debug('logger.debug test');
-		this.logger.warn('logger.warning test');
-		this.logger.log('logger.log test');
+		this.logger.debug({ spanId: 1, func: 'matching' }, 'logger.debug test');
+		this.logger.warn({ spanId: 2, func: 'matching' }, 'logger.warning test');
 		this.logger.verbose('logger.verbose test');
 
 		try {
@@ -23,5 +22,9 @@ export class MatchingService {
 		}
 		// const driverInfo = await this.matchingRepository.findWaitingDriver();
 		// return driverInfo;
+	}
+
+	logTest(): void {
+		// this.logger.debug('logger.debug test2');
 	}
 }
