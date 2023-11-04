58eced839617114f727cfd74e0acd2484f444bc3
xpecter <menistream@gmail.com>
2023-11-04 13:45:46 +0900

    feat: DB seed 추가

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app/app.module';
+import { SeedService } from './seed/seed.service';
+import { EntityNotFoundFilter } from './util/filter/typeorm-exception.filter';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	await app.init();
 
 	const configService = app.get(ConfigService);
 	const port = configService.get<number>('port');
 	const host = configService.get<string>('host');
+
+	const seedService = app.get(SeedService);
+	await seedService.seedUsers();
+	await seedService.seedDrivers();
+
+	app.useGlobalFilters(new EntityNotFoundFilter());
 
 	await app.listen(Number(port), host);
 
